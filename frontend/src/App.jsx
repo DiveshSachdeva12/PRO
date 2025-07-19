115e8bf56cc90800b6f53b1578ad485931b19b88
DiveshSachdeva12 <diveshkumarsachdeva85@gmail.com>
2025-07-19 16:43:08 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './Navbar';
 import ComplaintForm from './components/ComplaintForm';
 import StaffForm from './components/StaffForm';
 import KiteDistribution from './components/KiteDistribution';
-import AdminDashboard from "./components/AdminDashboard";  // ✅ adjust path if needed
 
 
 export default function App() {
@@ -14,11 +13,7 @@ export default function App() {
       <div style={{ padding: 20 }}>
         <Routes>
           <Route path="/complaints" element={<ComplaintForm />} />
-          <Route path="/staff" element={<StaffForm />} />
           <Route path="/kite-distribution" element={<KiteDistribution />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-
-
         </Routes>
       </div>
     </>
